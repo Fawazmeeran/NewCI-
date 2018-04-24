ed3af4ece37597bb1d2898b8e010a984852f8497
fawazmeeran <fawazmeeran@gmail.com>
2018-04-24 12:16:37 +0000

    fib

---
+++ fib.py
@@ -0,0 +1,11 @@
+n = input("How many number series:")
+def fib(n):
+     a, b = 0, 1
+     print (a)
+     print  (b)
+     for i in range(0, n):    
+             c= a+b
+            
+             a,b = b , c
+             print(c)
+fib(n)
