bcf93ebce2e72ae25118b3f6e2e93f7c6b11568f
Fawazmeeran <38691503+Fawazmeeran@users.noreply.github.com>
2018-04-25 17:46:07 +0530

    Update fib.py

--- fib.py
+++ fib.py
@@ -2,14 +2,14 @@ n = input("How many number series:")
 def fib(n):
      a, b = 0, 1
      print (a)
-     print  (b)
+     print (b)
      for i in range(0, n):    
              c= a+b
             
              a,b = b , c
              print(c)
 
-	     print("Yess")
+	     print("Yejhguhyss")
 
          
 fib(n)
